2ffa3e70311205028f53ab1374e7b6cc0adc7b8d
Hacking591 <gertlula2003@gmail.com>
2025-04-15 15:32:54 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sqlXXX.epizy.com"; // vendos hostin e databazës nga InfinityFree
-$user = "epiz_12345678";    // përdoruesi yt
-$pass = "password";         // fjalëkalimi
-$db = "epiz_12345678_smartdb"; // databaza
+$host = "sql210.infinityfree.com"; // vendos hostin e databazës nga InfinityFree
+$user = "if0_38755413";    // përdoruesi yt
+$pass = "CoKVJ4oOBj0k8";         // fjalëkalimi
+$db = "if0_38755413_smartdb"; // databaza
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
