36af261907917afe40644e31c82d06de94a92114
Hacking591 <gertlula2003@gmail.com>
2025-04-15 15:51:29 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,21 +1,7 @@
+// login.php (shumë e thjeshtë për testim)
 <?php
-include "db.php";
-
-$data = json_decode(file_get_contents("php://input"), true);
-$username = $conn->real_escape_string($data["username"]);
-$password = $data["password"];
-
-$sql = "SELECT * FROM users WHERE username='$username'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  $user = $result->fetch_assoc();
-  if (password_verify($password, $user['password'])) {
-    echo json_encode($user);
-  } else {
-    echo "Gabim";
-  }
-} else {
-  echo "Gabim";
-}
+  echo json_encode([
+    "username" => "testuser",
+    "role" => $_POST["role"]
+  ]);
 ?>
